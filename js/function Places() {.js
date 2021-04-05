0c57bcfb2e59b8c1bb2d40b0df3f8055009f5dd5
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-04-05 15:42:36 -0700

    added js file
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>

---
+++ js/function Places() {.js
@@ -0,0 +1,28 @@
+function Places() {
+  this.places = {};
+  this.currentId = 0;
+}
+
+Places.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Places.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
+}
+
+function Location(location, landmarks, timeOfYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+let places = new Places();
+let location = new Location("Florida", "DisneyWorld", "Fall", "Awesome!");
+let location2 = new Location("Colorado", "Xgames", "Winter", "Radical");
+places.addPlace(location);
+places.addPlace(location2);
+console.log(places.places);
